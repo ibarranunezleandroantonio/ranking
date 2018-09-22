7405aa06a9ab86d946c0e9fd30f9827cb680c910
ibarranunezleandroantonio <42699408+ibarranunezleandroantonio@users.noreply.github.com>
2018-09-22 11:07:29 -0300

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -17,7 +17,7 @@ closes.forEach(function(close){
         content.classList.add("animated");
         content.classList.add("fadeOutRight");
         setTimeout(() => {
-            location.href="/";
+            location.href="../";
         }, 500);
      });
 })
